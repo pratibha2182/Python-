7902554f7284d32dcee08a944e55fdf6d8513cef
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-11 14:13:33 +0000

    Variables.py

---
+++ variables.py
@@ -0,0 +1,28 @@
+# A variable is like a labelled box where we store data.
+name = "pratibha"  #string
+age = 22  #integer
+height = 5.4    #float
+isStudent = True     #boolen
+
+print (name)
+print(age)
+print(height)
+print(isStudent)
+
+print("\nType of variables")
+print (type(name))
+print(type(age))
+print(type(height))
+print(type(isStudent))
+
+print("\nType conversion")
+age_str = str(age)
+heght_int= int(height)
+
+print(age_str)
+print(heght_int)
+
+
+print("\n type after type conversion")
+print(type(age))
+print(type(height))
