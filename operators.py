10aeca8469c0a8808713137a33ec33ec2c4be2b0
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-11 14:23:10 +0000

    Operators.py

---
+++ operators.py
@@ -0,0 +1,32 @@
+'''
++ Addition / string concat
+
+- Subtraction
+
+* Multiplication
+
+/ Division (float)
+
+// Floor division (no decimal)
+
+% Modulus (remainder)
+
+** Exponentiation (power)
+
+'''
+
+a = 12
+b = 20
+print ("Addition: ", a+b )
+print ("Subtraction: ", a-b )
+print ("Multiplication: ", a*b )
+print ("Division: ", a/b )
+print ("Modulus : ", a%b )
+print("Floor division: ",a//b)
+print("Power: ", a**b)
+
+print("\nstring concat")
+c = "Hello"
+d = "World"
+e = c + d
+print(e)
