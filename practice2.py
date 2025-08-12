69e50e075d3b88ce9846ea55208654c32dc71cd3
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-12 08:25:20 +0000

    Practice 2

---
+++ practice2.py
@@ -0,0 +1,39 @@
+text = "Python Programming"
+print("Q1")
+print(text[0:6])
+
+fruits =  ["apple", "banana", "cherry", "mango", "orange"]
+print("\nQ2")
+print(fruits[0])
+print(fruits[-1])
+
+sentence = "I love Pyhton Programming"
+print("\nQ3")
+sen = list(sentence)
+print(sen)
+
+numbers = [10, 20, 30, 40, 50]
+print("\nQ4")
+numbers.append(60)
+print(numbers)
+numbers.insert(1, 15)
+print(numbers)
+
+word = "banana"
+print("\nQ5")
+word.count("a")
+print(word)
+
+data = [3, 1, 4, 2, 5]
+print("\nQ6")
+data.sort()
+print(data)
+
+data.sort(reverse=True)
+print(data)
+
+words = ["Hello", "World"]
+print("\nQ7")
+sent = "".join(words)
+print(sent)
+
