56c06fcf0d512a597189e3b2c2e43d18d00447a6
Pratibha Singh <133392459+pratibha2182@users.noreply.github.com>
2025-08-15 07:36:10 +0000

    Enumeratepy

---
+++ enumerate.py
@@ -0,0 +1,21 @@
+'''
+enumerate is a build in function allow you to loops over a sequence such as list,tuple,sting and get the index and value of each element in the sequence at the same time.
+'''
+
+'''
+fruits = ["apple", "banana","orange","cherry"]
+# for index, fruits in enumerate(fruits):
+#     print(index, fruits)
+
+# changing the start index
+for index, fruits in enumerate(fruits, start=1):
+    print(index, fruits)
+'''
+
+'''
+marks = [1,3,6,43,23,55,78,88]
+for index, mark in enumerate(marks):
+    print(mark)
+    if (index == 7):
+        print("highest marks")
+        '''
